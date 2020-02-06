838e0468c26f64e8950ce4b59ab4352ad33e80d7
cxf <1579814534@qq.com>
2020-02-06 17:52:05 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// element-ui完整导入
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// element-ui按需引入
+import './plugins/element'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 全局样式
+import './assets/css/global.css'
+
+// 图标库
+import './assets/fonts/iconfont.css'
+
+// 导入axios
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+// Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
