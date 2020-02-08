c587e9ebc2e4e1c30e58589dd2429baf8901c543
cxf <1579814534@qq.com>
2020-02-08 14:39:31 +0800

    完成用户列表功能开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,57 @@
 <template>
-    <el-container class="home-container">
-        <!-- 头部区域 -->
-        <el-header>
-            <div>
-                <img src="../assets/logo.png" alt="">
-                <span>vue_lms</span>
-            </div>
-            <el-button type="info" @click="logout">退出</el-button>
-        </el-header>
-        <!-- 页面主体区域 -->
-        <el-container>
-            <!-- 侧边栏 -->
-            <el-aside :width="isCollapse ? '64px' : '200px'">
-                <div class="toggle-button" @click="toggleCollapse">|||</div>
-                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
-                    <!-- 一级菜单 -->
-                    <el-submenu :index="item.id +''" v-for="item in menulist" :key="item.id">
-                        <template slot="title">
-                            <i :class="iconsObj[item.id]"></i>
-                            <span>{{item.authName}}</span>
-                        </template>
-                        <!-- 二级菜单 -->
-                        <el-menu-item @click="saveNavState('/' + subItem.path)" :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
-                            <template slot="title">
-                                <i class="el-icon-menu"></i>
-                                <span>{{subItem.authName}}</span>
-                            </template>
-                        </el-menu-item>
-                    </el-submenu>
-                </el-menu>
-            </el-aside>
-            <!-- 右侧内容主体 -->
-            <el-main>
-                <!-- router占位符 -->
-                <router-view></router-view>
-            </el-main>
-        </el-container>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png"
+             alt="">
+        <span>vue_lms</span>
+      </div>
+      <el-button type="info"
+                 @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button"
+             @click="toggleCollapse">|||</div>
+        <el-menu background-color="#333744"
+                 text-color="#fff"
+                 active-text-color="#409EFF"
+                 unique-opened
+                 :collapse="isCollapse"
+                 :collapse-transition="false"
+                 router
+                 :default-active="activePath">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id +''"
+                      v-for="item in menulist"
+                      :key="item.id">
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item @click="saveNavState('/' + subItem.path)"
+                          :index="'/' + subItem.path"
+                          v-for="subItem in item.children"
+                          :key="subItem.id">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- router占位符 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,35 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <!-- 头像区域 -->
-            <div class="avatar_box">
-                <img src="../assets/logo.png" alt="">
-            </div>
-            <!-- 表单区域 -->
-            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
-                <el-form-item prop="username">
-                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
-                </el-form-item>
-                <el-form-item prop="password">
-                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
-                </el-form-item>
-                <el-form-item class="btns">
-                    <el-button type="primary" @click="login">登陆</el-button>
-                    <el-button type="info" @click="resetForm">重置</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png"
+             alt="">
+      </div>
+      <!-- 表单区域 -->
+      <el-form ref="loginFormRef"
+               :model="loginForm"
+               :rules="loginFormRules"
+               label-width="0px"
+               class="login_form">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username"
+                    prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password"
+                    prefix-icon="iconfont icon-3702mima"
+                    type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary"
+                     @click="login">登陆</el-button>
+          <el-button type="info"
+                     @click="resetForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
